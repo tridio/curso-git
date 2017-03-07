51f3789af6a32117c98f5d9cb35e8fdf3e6ae52f
joaoCunha <guto202@gmail.com>
2017-03-06 23:42:33 -0300

    commit01

--- src/regex/gitTest.java
+++ src/regex/gitTest.java
@@ -1,5 +1,18 @@
 package regex;
 
 public class gitTest {
-
+	public static void main(String args[]) {
+		try {
+			String arr[] = new String[10];
+			arr = null;
+			arr[0] = "one";
+			System.out.print(arr[0]);
+		} catch (Exception ex) {
+			System.out.print("exception");
+		} catch (NullPointerException nex) {
+			System.out.print("null pointer exception");
+		}
+		"teste".split("\s");
+	}
+	
 }
